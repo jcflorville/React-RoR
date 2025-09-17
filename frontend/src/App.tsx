ec16a39bc811741adda68253ac9af4007b020c33
Juan Florville <jcflorville@gmail.com>
2025-09-17 15:54:24 -0300

    feat: Installing Flowbite

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from "react"
 import reactLogo from "./assets/react.svg"
 import viteLogo from "/vite.svg"
 import "./App.css"
+import { Button } from "flowbite-react"
 
 function App() {
 	const [count, setCount] = useState(0)
@@ -9,6 +10,8 @@ function App() {
 	return (
 		<>
 			<h1 className='text-3xl font-bold underline'>Hello world!</h1>
+			<Button>Default</Button>
+			<Button color='red'>Alternative</Button>
 		</>
 	)
 }
